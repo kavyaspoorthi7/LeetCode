47492e1af61f47d5f5ff7ff2c1e70951733ed3af
kavyaspoorthi7 <spoorthikavya13@gmail.com>
2026-08-13 18:51:00 +0530

    Time: 41 ms (51.19%), Space: 12.5 MB (46.02%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        ans=[0]*(2*n)
+        for i in range(2*n):
+            if i%2==0:
+                ans[i]=nums[i//2]
+            else:
+                ans[i]=nums[n+i//2]
+        return ans
